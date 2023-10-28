cb497ed3f99220c0e683913bed32498a629cc564
Ihor <ihorrykun@gmail.com>
2023-10-28 23:09:58 +0300

    update section whats and customer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-(() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
-
-  menuBtnRef.addEventListener('click', () => {
-    const expanded =
-      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-    menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-    mobileMenuRef.classList.toggle('is-open');
-  });
-})();
+import './js/modal';
