2f70397815be675cbaacdb2799fb96a0e7d8c54f
IhorRykun <IhorRykun@gmail.com>
2023-11-24 23:23:06 +0200

    update modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,8 @@
 (() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
+  const menuBtnRef = document.querySelector('.modal__button');
+  const mobileMenuRef = document.querySelector('.wraper');
 
   menuBtnRef.addEventListener('click', () => {
-    const expanded =
-      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-    menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-    mobileMenuRef.classList.toggle('is-open');
+    mobileMenuRef.classList.toggle('veritatis-nan');
   });
 })();
