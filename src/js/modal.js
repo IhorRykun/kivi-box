8bd63e7ff665ce561a96f4c033775b46abcf3155
IhorRykun <IhorRykun@gmail.com>
2023-12-04 22:42:57 +0200

    add modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,11 @@
 (() => {
   const menuBtnRef = document.querySelector('.wraper');
   const mobileMenuRef = document.querySelector('.main__button');
+  const btnClose = document.querySelector('.button__close');
   mobileMenuRef.addEventListener('click', () => {
+    menuBtnRef.classList.toggle('display-nan');
+  });
+  btnClose.addEventListener('click', () => {
     menuBtnRef.classList.toggle('display-nan');
   });
 })();
